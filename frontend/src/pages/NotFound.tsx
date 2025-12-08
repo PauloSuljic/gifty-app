79c17136d8c9c7261cb0a1f7c729b9159bfa5130
Paulo Suljic <paulo.suljic@gmail.com>
2025-12-08 01:20:39 +0100

    Implement 404 Not Found handling and improve error handling in WishlistDetails (#82)
    Co-authored-by: paulo_suljic <paulo@matchwornshirt.com>

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white px-4">
-      <h1 className="text-5xl md:text-6xl font-bold text-red-500">404</h1>
+    <div className="pt-20 flex flex-col items-center justify-center bg-gray-900 text-white px-4">
+      <h1 className="text-5xl md:text-6xl font-bold text-purple-500">404</h1>
       <p className="text-lg md:text-xl mt-4 text-center">
         Oops! The page you’re looking for doesn’t exist.
       </p>
       <Link
         to="/dashboard"
-        className="mt-6 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-md shadow-md transition"
+        className="mt-6 px-6 py-3 bg-purple-700 hover:bg-blue-500 text-white rounded-md shadow-md transition"
       >
         Go Home
       </Link>
