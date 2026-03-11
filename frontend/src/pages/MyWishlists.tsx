9f3e86809a1a29706951036d2113bbd4f081af7a
Paulo Suljic <paulo@matchwornshirt.com>
2026-03-11 20:34:12 +0100

    feat: add My Wishlists page and update sidebar navigation

---
+++ frontend/src/pages/MyWishlists.tsx
@@ -0,0 +1,11 @@
+import Wishlist from "../components/Wishlist";
+
+const MyWishlists = () => {
+  return (
+    <div className="mt-6">
+      <Wishlist />
+    </div>
+  );
+};
+
+export default MyWishlists;
